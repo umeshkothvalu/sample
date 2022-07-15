94ff8fa097fdf18885064bdb6b0d8b0fd72b936b
EC2 Default User <ec2-user@ip-172-31-35-122.ap-south-1.compute.internal>
2022-07-15 08:56:23 +0000

    CM2 Added changes to HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
 	public static void main(String ar[]){
 		System.out.println("Hello World!!");
+		System.out.println("Welcome to the Jenkins World!!");
+		System.out.println("Jenkins Helps in Automation !!");
+		System.out.println("Lets Start!!");
+		System.out.println("!!!!!!!!!!!");
 	}
 }
